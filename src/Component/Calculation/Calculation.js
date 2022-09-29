e7f3eeb885f2fdf0a9958f206689b6997500fb88
unknown <nazmulnobel000@gmail.com>
2022-09-29 23:27:58 +0600

    break time add

--- src/Component/Calculation/Calculation.js
+++ src/Component/Calculation/Calculation.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculation.css'
 import image from './player-3.png'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Calculation = (props) => {
+    const [breakTime, setBreakTime] = useState(0);
     const {time} = props;
     let newminite = 0;
     for(const minite of time){
@@ -14,6 +15,10 @@ const Calculation = (props) => {
     const diftToast = () => {
         toast.success("Thank you for visite my website",
         {position:"top-center"});
+    }
+
+    const breakTimeAdd = (min) =>{
+        setBreakTime(min);
     }
 
     return (
@@ -42,16 +47,16 @@ const Calculation = (props) => {
 
             <h1>Add A Break</h1>
             <div className='time'>
-                <a href="/a">10</a>
-                <a href="/a">20</a>
-                <a href="/a">30</a>
-                <a href="/a">40</a>
-                <a href="/a">50</a>
+                <a onClick={() => breakTimeAdd(10)} href="/a">10 M</a>
+                <a onClick={() => breakTimeAdd(20)} href="/a">20 M</a>
+                <a onClick={() => breakTimeAdd(30)} href="/a">30 M</a>
+                <a onClick={() => breakTimeAdd(40)} href="/a">40 M</a>
+                <a onClick={() => breakTimeAdd(50)} href="/a">50 M</a>
             </div>
 
             <h1>Exercise Details</h1>
             <h3 className='exercise-time'>Exercise time: {newminite} M</h3>
-            <h3 className='exercise-time'>Break time: 0 S</h3>
+            <h3 className='exercise-time'>Break time: {breakTime} S</h3>
             <button onClick={diftToast} className='complete'>Activity Complete <ToastContainer /></button>
 
         </div>
